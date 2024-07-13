correct confidence band calculation

scripts/get_confidence.py
@@ -9,8 +9,6 @@ import pandas as pd
 
 from fork_env.constants import (
     DATA_FOLDER,
-    HASH_STD,
-    SIMULATED_FORK_RATES_PATH,
     EMPIRICAL_PROP_DELAY,
     N_MINER,
     SUM_HASHES,
@@ -54,41 +52,43 @@ def fork_temp(
     )
 
 
-# open file for writing by appending
-with open(DATA_FOLDER / "rates_confidence.jsonl", "a") as f:
-    for dist_key, dist in {
-        "log_normal": fork_rate_ln,
-        "lomax": fork_rate_lomax,
-    }.items():
-        for sumhash in SUM_HASHES:
-            hash_mean = sumhash / N_MINER
-            hash_var = (
-                sum([(p * sumhash) ** 2 for p in ps]) - N_MINER * hash_mean**2
-            ) / (N_MINER - 1)
-            mean_std = np.sqrt(sum_var_p) * hash_mean
-            var_std = np.sqrt(2 / (N_MINER * (N_MINER - 1)) * sum_var_p2) * hash_mean
-
-            for delta in list(EMPIRICAL_PROP_DELAY.values()):
-                results = Parallel(n_jobs=-1)(
-                    delayed(fork_temp)(
-                        hash_mean, mean_std, hash_var, var_std, delta, dist
-                    )
-                    for _ in range(int(1e4))
+if __name__ == "__main__":
+    # open file for writing by appending
+    with open(DATA_FOLDER / "rates_confidence.jsonl", "a") as f:
+        for dist_key, dist in {
+            "log_normal": fork_rate_ln,
+            "lomax": fork_rate_lomax,
+        }.items():
+            for sumhash in SUM_HASHES:
+                hash_mean = sumhash / N_MINER
+                hash_var = (
+                    sum([(p * sumhash) ** 2 for p in ps]) - N_MINER * hash_mean**2
+                ) / (N_MINER - 1)
+                mean_std = np.sqrt(sum_var_p) * hash_mean
+                var_std = (
+                    np.sqrt(2 / (N_MINER * (N_MINER - 1)) * sum_var_p2) * hash_mean
                 )
-                this_rate = {
-                    "dist": dist_key,
-                    "sumhash": sumhash,
-                    "proptime": delta,
-                    "mean_mean": hash_mean,
-                    "mean_std": mean_std,
-                    "mean_var": hash_var,
-                    "var_std": var_std,
-                    "results": results,
-                }
-                print(this_rate)
-        # write to jsonl
-        f.write(json.dumps(this_rate) + "\n")
 
+                for delta in list(EMPIRICAL_PROP_DELAY.values()):
+                    results = Parallel(n_jobs=-1)(
+                        delayed(fork_temp)(
+                            hash_mean, mean_std, hash_var, var_std, delta, dist
+                        )
+                        for _ in range(int(1e3))
+                    )
+                    this_rate = {
+                        "dist": dist_key,
+                        "sumhash": sumhash,
+                        "proptime": delta,
+                        "mean_mean": hash_mean,
+                        "mean_std": mean_std,
+                        "mean_var": hash_var,
+                        "var_std": var_std,
+                        "results": results,
+                    }
+                    print(this_rate)
 
-time_taken = time.time() - start_time
-print(f"Time taken: {time_taken}")
+                    f.write(json.dumps(this_rate) + "\n")
+
+    time_taken = time.time() - start_time
+    print(f"Time taken: {time_taken}")

scripts/run_fork_rates_analytical_ln.py
@@ -25,7 +25,8 @@ def compute_rate(args) -> tuple[tuple, float]:
             std=std,
         )
     except Exception as e:
-        print(f"Error in {args}: {e}")
+        # don't care -- too many prints otherwise
+        # print(f"Error in {args}: {e}")
         the_rate = np.nan
     print(args, the_rate)
     return args, the_rate