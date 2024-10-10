f6a3aba9704a9d77d2252142a78ef933aa56a6b4
jiahuaxu <dsf-pro16-m3@DSF-pro16-m3s-MacBook-Pro.local>
2024-10-10 21:46:31 +0100

    add tpl fork rate calculation

---
+++ fork_env/integration_tpl.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy.integrate import quad_vec
+
+from fork_env.constants import HASH_STD
+from fork_env.utils import (
+    calc_truncpl_params,
+)
+
+
+def fork_rate_tpl(
+    proptime: float,
+    n: int,
+    sum_lambda: float | None = None,
+    hash_mean: float | None = None,
+    std: float = HASH_STD,
+    **kwargs,
+) -> float:
+    if hash_mean is None:
+        hash_mean = sum_lambda / n
+    alpha, beta, _ = calc_truncpl_params(hash_mean=hash_mean, hash_std=std)
+
+    def pdelta_integrand(x: float) -> float:
+        return (beta + x) ** (-2 + alpha) * (beta + proptime + x) ** (
+            (alpha - 1) * (n - 1)
+        )
+
+    return (
+        1
+        - n
+        * (1 - alpha)
+        * beta ** (n * (1 - alpha))
+        * quad_vec(
+            pdelta_integrand,
+            0,
+            np.inf,
+        )[0]
+    )
+
+
+if __name__ == "__main__":
+    res = fork_rate_tpl(
+        proptime=1,
+        hash_mean=0.31 / 42,
+        n=42,
+        std=0.06,
+    )
+    print(res)
