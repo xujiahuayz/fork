c9cb04407867604d95539a1361a81207b7738785
jiahuaxu <dsf-pro16-m3@DSF-pro16-m3s-MacBook-Pro.local>
2024-11-03 18:43:52 +0000

    clean up plot  code

--- scripts/plot_id.py
+++ scripts/plot_id.py
@@ -17,33 +17,16 @@ rates.columns = ["iid", "block_propagation_time", "sum_hash", "rate"]
 plt.rcParams.update({"font.size": 15})
 
 small_x = 180
-small_y = 0.38
-# shade out a square of x=0, y=0 to x=100, y=0.14 with grey color
-# plt.fill_between(
-#     [0, small_x, small_x, 0],
-#     [0, 0, small_y, small_y],
-#     color="grey",
-#     alpha=0.1,
-# )
 
 
 for i, sum_hash in enumerate(SUM_HASHES):
     df = rates[rates["sum_hash"] == sum_hash]
     df = df.sort_values(by="block_propagation_time")
-    # plot iid true and iid false
-    # df_iid = df[df["iid"] == True]
-    # df_not_iid = df[df["iid"] == False]
 
     for j, id in enumerate([None, False, True]):
         # get the fork rate for id is None
         df_iid = df[(df["iid"].isnull()) if id is None else (df["iid"] == id)]
 
-        # plt.plot(
-        #     df_not_iid["block_propagation_time"],
-        #     df_not_iid["rate"],
-        #     label="independent",
-        #     color="black",
-        # )
         plt.plot(
             df_iid["block_propagation_time"],
             df_iid["rate"],
@@ -62,8 +45,6 @@ for i, sum_hash in enumerate(SUM_HASHES):
         ],
         color="red",
         label="$p(0\\vert \\{\\lambda_i\\})$",
-        # \\Lambda \\cdot \\left[1-  \\sum_i \\left(\\frac{b_i}{B} \\right)^2 \\right] $",
-        # linewidth=2,
     )
 
     plt.text(
@@ -75,7 +56,6 @@ for i, sum_hash in enumerate(SUM_HASHES):
     )
     plt.xlabel("block propagation time $\\Delta_0$ [s]")
     plt.ylabel("fork rate $C(\\Delta_0)$")
-    # yaxis
     plt.xlim(0, 1100)
     plt.ylim(0, 1.19)
 
@@ -83,72 +63,8 @@ for i, sum_hash in enumerate(SUM_HASHES):
     if i == 0:
         plt.legend(loc="upper left", frameon=False, handlelength=1)
 
-    # add legend on bottom right
-
-    # plt.show()
-
 
 plt.tight_layout()
 plt.savefig(FIGURES_FOLDER / f"fork_rate_id.pdf")
 
 plt.show()
-
-
-# sum_hash = SUM_HASHES[1]
-# df = rates[rates["sum_hash"] == sum_hash]
-# df = df.sort_values(by="block_propagation_time")
-# df_not_iid = df[df["iid"].isnull()]
-
-
-# plt.fill_between(
-#     [0, small_x, small_x, 0],
-#     [0, 0, small_y, small_y],
-#     color="grey",
-#     alpha=0.1,
-# )
-
-# plt.plot(
-#     [0, small_x],
-#     [
-#         0,
-#         sum_hash * (1 - sum((b / BLOCK_WINDOW) ** 2 for b in BIS)) * small_x,
-#     ],
-#     color="blue",
-#     linewidth=2,
-# )
-# # write d c / d t = 0 in parrallel to the theoretical line
-# plt.text(
-#     0,
-#     0.25,
-#     "$\\left( \\frac{d C(\\Delta_0)}{d \\Delta_0} \\right)_{\\Delta_0 = 0} = \\Lambda \\cdot \\left[1-  \\sum_i \\left(\\frac{b_i}{B} \\right)^2 \\right] $",
-#     horizontalalignment="left",
-#     verticalalignment="top",
-#     rotation=37,
-#     color="blue",
-# )
-# plt.plot(
-#     df_not_iid["block_propagation_time"],
-#     df_not_iid["rate"],
-#     # label="i.i.d.",
-#     color="black",
-# )
-
-# plt.text(
-#     230,
-#     small_y - 0.12,
-#     f"$\Lambda = {round(sum_hash,4)}$",
-#     horizontalalignment="left",
-#     verticalalignment="top",
-# )
-
-# # plot a line y = k * x, k = 1 - sum(bis^2)/sum(bis)^2
-
-
-# plt.xlabel("block propagation time $\\Delta_0$ [s]")
-# plt.ylabel("fork rate $C(\\Delta_0)$")
-# # yaxis
-# plt.xlim(0, small_x)
-# plt.ylim(0, small_y)
-
-# plt.tight_layout()
-# plt.savefig(FIGURES_FOLDER / f"fork_slope.pdf")
